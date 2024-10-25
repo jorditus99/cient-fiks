f6d1d8f3d3c92f96fa1b24df3f04d2730e2031f0
CarlaCayero <ccaarlaa@gmail.com>
2024-10-25 19:44:41 +0200

    Animation background image

--- joc_roger/index.php
+++ joc_roger/index.php
@@ -10,6 +10,7 @@
 <body>
     <div class="image-container">
         <img id="img_principal" src="../img/img_roger/fons_roger.png" alt="img_fons" id="">
+        <img id="img_principal" src="../img/img_roger/fons_roger.png" alt="img_fons" id="">
     </div>
 </body>
 </html>
