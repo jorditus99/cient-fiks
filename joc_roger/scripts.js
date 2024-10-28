3f17ae42d46a7510e66c7e8948477b606a7db2ce
CarlaCayero <ccaarlaa@gmail.com>
2024-10-28 19:17:19 +0100

    Movement character

--- joc_roger/scripts.js
+++ joc_roger/scripts.js
@@ -1,14 +1,17 @@
-const personatge = document.getElementById('personatge');
-
-
+const personatge = document.getElementById("personatge");
+let posicioTop = 0;
 console.log(personatge.src);
 
 function moure_personatge(event) {
     switch (event.key) {
         case 'ArrowUp':
+            posicioTop -= 210; // Mou cap amunt
+            personatge.style.top = posicioTop + "px";
             console.log("Fletxa damunt");
             break;
         case 'ArrowDown':
+            posicioTop += 210; // Mou cap avall
+            personatge.style.top = posicioTop + "px";
             console.log("Fletxa abaix");
             break;
         default:
