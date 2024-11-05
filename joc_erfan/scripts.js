1abeb81e32c92f2338b18e51b86ba65dfdef962a
ErfanCEP <rribasm2324@politecnics.barcelona>
2024-11-05 20:23:10 +0100

    lletres incorporades
    gotes incorporades a les gotes correctament, queda fraccionar el metode crear gota

--- joc_erfan/scripts.js
+++ joc_erfan/scripts.js
@@ -1,10 +1,18 @@
 let gotas = [];
 let vides = [];
+let lletres = [];
+let lletres_no_potables = [];
+
 
 //Creació de gota i els seus parametres
 
 function crear_gota() {
 
+    // Crear contenidor per la gota i la lletra -- Funció crear_container_gota
+    let container_gota = document.createElement("div")
+    container_gota.className = "container_gota"
+
+    //Crear la gota
     let gota = document.createElement('img')
     gotas.push(gota);
     gota.className = "gota"
@@ -17,8 +25,34 @@ function crear_gota() {
     fons.appendChild(gota);
     gota.setAttribute('data-potable', 'true');
 
+    //Crear imatge lletra random  -- Funció crear aparença de lletra random
+   
+    let lletra = patata(left);
+    
+
+    //Ficar gota i lletra al container_gota -- Funció?
+    container_gota.appendChild(gota);
+    container_gota.appendChild(lletra);
+
+
+    //Afegir el container_gota al fons -- Dins de crear_container_gota 
+    document.getElementById("fons").appendChild(container_gota);
 
 }
+
+function patata (left) {
+
+    let lletra = document.createElement("span");
+    lletra.className = "lletra_gota"
+    lletra.innerText = lletra_random();
+    lletra.style.position = "absolute;"
+    lletra.style.top = "160px";
+    lletra.style.left = left + "px";
+
+    return lletra;
+    
+}
+
 
 
 function crear_no_potable() {
@@ -34,8 +68,13 @@ function crear_no_potable() {
     let fons = document.getElementById("fons");
     fons.appendChild(gota2);
     gota2.setAttribute('data-potable', 'false');
+    
+}
 
+function lletra_random() {
 
+    const lletres = "QWERTYUIOPASDFGHJKLÑÇZXCVBNM"
+    return lletres.charAt(Math.floor(Math.random() *  lletres.length));
 
 }
 
@@ -56,6 +95,7 @@ function joc() {
 
     let interval = setInterval(() => {
         moure_gota();
+        moure_lletra();
         colisio();
     }, 100)
 }
@@ -65,7 +105,21 @@ function moure_gota() {
         position = parseInt(gota.style.top);
         position += 5;
         gota.style.top = position + "px";
+
+        let lletra = gota.nextSibling; // Obtener el elemento de letra
+        if (lletra) {
+            lletra.style.top = position + "px"; // Mover la letra con la gota
+        }
     }
+}
+
+function moure_lletra() {
+    for (lletra of lletres) {
+        positon = parseInt(lletra.style.top);
+        position += 5;
+        lletra.style.top = position + "px";
+    }
+
 }
 
 function colisio() {
@@ -80,6 +134,10 @@ function colisio() {
             }
             gota.remove();
             gotas.splice(i, 1);
+
+            //No funciona
+            lletra.remove();
+            lletres.splice(i,1);
         }
     }
 }
