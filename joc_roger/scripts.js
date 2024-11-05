2bc097b9dbd7d7096e372b0157d122a316a4161b
RogerOrti <rortim2223@politecnics.barcelona>
2024-11-05 19:07:15 +0100

    Obstacle movement

--- joc_roger/scripts.js
+++ joc_roger/scripts.js
@@ -1,6 +1,7 @@
 const imatge_fons = document.getElementById("image_container");
 let posicioTop = 0;
 let obstacles = ["../img/img_roger/petroli.png", "../img/img_roger/llauna.png", ""];
+let velocitat = 10;
 let vides = [];
 
 // Creació del personatge
@@ -37,17 +38,34 @@ function moure_personatge(event) {
 
 // Creació dels obstacles
 function crear_obstacle() {
-    const img_petroli = document.createElement('img');
+    let img_petroli = document.createElement('img');
     img_petroli.src = "../img/img_roger/petroli.png";
     img_petroli.alt = "Obstacle petroli";
     img_petroli.style.position = "absolute"; 
-    img_petroli.style.top = "100px"; 
-    img_petroli.style.left = "200px"; 
+    // img_petroli.style.top = "50px"; 
+    img_petroli.style.left = "1000px"; 
     img_petroli.style.width = "100px"; 
 
     imatge_fons.appendChild(img_petroli);
+
+    moure_obstacle(img_petroli);
 }
 
+
+function moure_obstacle(img_petroli) {
+    let posicioX = parseInt(img_petroli.style.left); 
+
+    // Moure la imatge cada 10 ms
+    const moviment = setInterval(() => {
+        posicioX -= velocitat;
+        img_petroli.style.left = posicioX + 'px';
+
+        // Quan la imatge surt de la pantalla, la tornem a l'inici
+        if (posicioX < -img_petroli.width) {
+            posicioX = window.innerWidth;
+        }
+    }, 5);
+}
 // Inicialització
 crear_personatge();
 crear_obstacle();
