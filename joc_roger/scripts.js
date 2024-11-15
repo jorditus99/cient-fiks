62235ee2c507244bc50e255e483a4f9a344294e2
RogerOrti <rortim2223@politecnics.barcelona>
2024-11-15 16:38:49 +0100

    Add scor on game

--- joc_roger/scripts.js
+++ joc_roger/scripts.js
@@ -187,16 +187,11 @@ function definir_vides(numVides) {
 
 function afegir_puntuacio() {
 
-    if (vides > 0) {
-        const interval = setInterval(() => {
-            if (vides > 0) {
-                puntuacio += 5;
-                punts.innerHTML = puntuacio;
-            } else {
-                clearInterval(interval); // Parar l'interval quan no hi ha més vides
-            }
-        }, 5000); // Cada 5 segons
-    }
+    do {
+        puntuacio += 5;
+        console.log(puntuacio);
+        punts.innerHTML = puntuacio;
+    } while (vides > 0);
     
     
 }
@@ -212,7 +207,7 @@ function iniciar_joc() {
     moure_img_principal();
     crear_personatge();
     setInterval(crear_obstacle, 1000);
-    afegir_puntuacio();
+    setInterval(afegir_puntuacio, 5000) ;
   }
   
   // Per iniciar el joc, només caldria cridar
