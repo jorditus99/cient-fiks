5793a76c0b387a2ae6e338cd767e1bf5564223de
jorditus99 <JORDIGREVOL@GMAIL.COM>
2024-11-22 19:17:37 +0100

    php acabado perrooooooss

--- php_library/library.php
+++ php_library/library.php
@@ -2,102 +2,104 @@
 
 
 
-function connectionDB(){
+function connectionDB()
+{
 
     $servername = "localhost";
     $username = "root";
     $pass = "mysql";
- 
-    try{
-    $connection = new PDO("mysql:host=$servername;dbname=cientifiks", $username, $pass);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=cientifiks", $username, $pass);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $connection;
-
-    }  catch (PDOException $e){
-        die('Error: '.$e->getMessage());
-    } 
+    } catch (PDOException $e) {
+        die('Error: ' . $e->getMessage());
     }
+}
 
 
-    function secure_data($data){
-    
-        // trim quita los espacios,  strip quita las comillas 
-        // y html subnota caracteres especiales
-        
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        
-        return $data;
-    
-    }
+function secure_data($data)
+{
+
+    // trim quita los espacios,  strip quita las comillas 
+    // y html subnota caracteres especiales
+
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+
+    return $data;
+}
 
 
-    function hash_pass($pass){
-        return password_hash($pass, PASSWORD_DEFAULT);
-    }
+function hash_pass($pass)
+{
+    return password_hash($pass, PASSWORD_DEFAULT);
+}
 
-    function create_user($name, $pass){
-        $nom = secure_data($name);
-        // $pass = secure_data($pass);
-        // $pass = hash_pass($pass);
+function create_user($name, $pass)
+{
+    $nom = secure_data($name);
+    $pass = secure_data($pass);
+    $pass = hash_pass($pass);
 
-        $connection = connectionDB();
+    $connection = connectionDB();
 
-        $stmt = $connection->prepare('Insert into usuario 
+    $stmt = $connection->prepare('Insert into usuario 
         (nom_usuario,  contrasenya) 
         VALUES  (:nom_usuario, :password)');
 
-         $stmt->bindParam(':nom_usuario', $nom);
-         $stmt->bindParam(':password', $pass);
-         $stmt->execute();
+    $stmt->bindParam(':nom_usuario', $nom);
+    $stmt->bindParam(':password', $pass);
+    $stmt->execute();
+}
 
-         
-    }
 
-    
 function closeBD()
 {
     return null;
-};
+}
 
-function check_nom($nom){
+function check_user($nom, $pass)
+{   $connection = connectionDB();
     $nom = secure_data($nom);
-    $connection = connectionDB();
-    $stmt = $connection->prepare('SELECT nom_usuario, contrasenya FROM usuario WHERE nom_usuario = :nom');
-    $stmt->bindParam('nom',$nom);
+    $stmt = $connection->prepare('SELECT * FROM usuario WHERE nom_usuario = :nom' );
+    $stmt->bindParam(':nom', $nom);
     $stmt->execute();
 
-    $result= $stmt->fetch();
-    if(isset($result['nom'])){
-        return true;
-    } else{
-        return false;
+    $result = $stmt->fetch(PDO::FETCH_ASSOC); 
+    if ($result && password_verify($pass, $result['contrasenya'])) {
+        return true; 
     }
-
-
+    return false; 
 }
 
+    
 
-function get_pass($nom){
-$nom = secure_data($nom);
-$connection = connectionDB();
-$stmt = $connection->prepare('SELECT nom_usuario, contrasenya FROM usuario WHERE nom_usuario = :nom');
-$stmt->bindParam('nom',$nom);
-$stmt->execute();
 
-$result= $stmt->fetch();
 
-return $result['password'];
-}
+// function get_pass($nom)
+// {
+//     $nom = secure_data($nom);
+//     $connection = connectionDB();
+//     $stmt = $connection->prepare('SELECT nom_usuario, contrasenya FROM usuario WHERE nom_usuario = :nom');
+//     $stmt->bindParam('nom', $nom);
+//     $stmt->execute();
 
-function auth_user($nom, $password){
-    $nom = secure_data($nom);
-    $password =secure_data($password);
+//     $result = $stmt->fetch();
 
-    if(check_nom($nom)){
-        $passInDB = get_pass($nom);
-        $resultAuth = password_verify($password, $passInDB);
-        return $resultAuth;
-    }
-}
+//     return $result['contrasenya'];
+// }
+
+// function auth_user($nom, $password)
+// {
+//     $nom = secure_data($nom);
+//     $password = secure_data($password);
+
+//     if (check_nom($nom)) {
+//         $passInDB = get_pass($nom);
+//         $resultAuth = password_verify($password, $passInDB);
+//         return $resultAuth;
+//     }
+// }
